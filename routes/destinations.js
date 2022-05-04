1dcd3a49dcec1769a92db96a803503783a145da4
GaetanHHHHHH <gaetanherinne@gmail.com>
2022-05-04 16:01:25 +0200

    Add first entry to DB

--- routes/destinations.js
+++ routes/destinations.js
@@ -7,6 +7,7 @@ const { sequelize } = require('../models');
 var models = require('../models'); // loads index.js
 var dest = models.Destination;       // the model keyed by its name
 
+// Get destinations list
 router.get('/', (req, res) => 
     dest.findAll()
         .then(destinations => {
@@ -14,5 +15,25 @@ router.get('/', (req, res) =>
             res.sendStatus(200)
         })
         .catch(err => console.log(err)));
+
+// Add a destination
+router.get('/add', (req, res) => {
+    const data = {
+        name: 'Looking for a holiday special',
+        country: 'Belgium',
+        description: 'Please I need a vacation'
+    }
+
+    let { name, country, description } = data;
+
+    // Insert into table
+    dest.create({
+        name,
+        country,
+        description
+    })
+    .then(dest => res.redirect('/destinations'))
+    .catch(err => console.logg(err));
+});
 
 module.exports = router;
