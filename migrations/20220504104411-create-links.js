864ca6c94bd5b2e4ef8d164c73e4c368a4656c24
GaetanHHHHHH <gaetanherinne@gmail.com>
2022-05-11 17:44:01 +0200

    Major updates

--- migrations/20220504104411-create-links.js
+++ migrations/20220504104411-create-links.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('links', {
+    await queryInterface.createTable('Lins', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,10 +9,12 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       iddestination: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        foreignKey: true
       },
       idagence: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        foreignKey: true
       },
       createdAt: {
         allowNull: false,
@@ -25,6 +27,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('links');
+    await queryInterface.dropTable('Link');
   }
 };
--- routes/destinations.js
+++ routes/destinations.js
@@ -7,18 +7,70 @@ const { sequelize } = require('../models');
 var models = require('../models'); // loads index.js
 var dest = models.Destination;       // the model keyed by its name
 
+
+
 // Get destinations list
-router.get('/', (req, res) => 
+
+// router.get('/', (req, res) => 
+//     dest.findAll()
+//         .then(destinations => {
+//             res.render('destinations', {
+//                 destinations
+//             })
+//         })
+//         .catch(err => console.log(err)));
+
+router.get('/', (req, res) =>
     dest.findAll()
         .then(destinations => {
-            res.render('destinations', {
-                destinations
-            })
+            const dests = {
+                context: destinations.map(data => {
+                    return {
+                        // id: data.id,
+                        name: data.name,
+                        description: data.description,
+                        country: data.country
+                    }
+                })
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(dests.context));
+            // res.render('destinations',{
+            //     destinations: dests.context
+            // });
+            // res.json(dests.context)
         })
-        .catch(err => console.log(err)));
+        .catch(err => res.status(500).json({ message: err }))
 
-// Display add destination form
-router.get('/add', (req, res) => res.render('add'));
+);
+
+
+// Return a destination with the id
+router.get('/id=:destId',(req,res)=>{
+    destId = req.params.destId;
+
+    //TODO: get the agencies with a request to /search/id=destId
+
+    dest.findByPk(destId)
+        .then( destination => {
+            const dest = {
+                context: {                    
+                    // id: destination.id,
+                    name: destination.name,
+                    description: destination.description,
+                    country: destination.country
+                }
+            }
+            res.json(dest.context)
+            // res.render('destinations',{
+            //     destinations: [dest.context]
+            // });
+        })
+        .catch(err => res.status(500).json({message: err}));
+   
+});
+
+
 
 // Add a destination
 router.post('/add', (req, res) => {
@@ -39,5 +91,12 @@ router.post('/add', (req, res) => {
     .then(dest => res.redirect('/destinations'))
     .catch(err => console.logg(err));
 });
+
+
+
+
+
+
+
 
 module.exports = router;
